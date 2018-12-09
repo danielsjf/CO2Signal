967a61d2aedc0fdf0c4c0b4b7107c16abf9c76ad
danielsjf <jef.daniels1@gmail.com>
2018-12-09 22:47:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from distutils.core import setup
 
 setup(
     name='CO2Signal',
+    description='A package to access the co2signal API',
+    url='https://github.com/danielsjf/CO2Signal',
     version='0.1',
     packages=['CO2Signal', ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
