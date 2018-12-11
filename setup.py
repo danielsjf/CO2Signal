578604ae0f48bb30dd3cd5f42a9f3a74ace8fa97
danielsjf <danielsjf@users.noreply.github.com>
2018-12-11 18:31:44 +0100

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='CO2Signal',
     description='A package to access the co2signal API',
     url='https://github.com/danielsjf/CO2Signal',
-    version='0.1',
+    version='0.2',
     packages=['CO2Signal', ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
